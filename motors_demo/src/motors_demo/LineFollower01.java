29c3f1488cffe9ca26caf0d407a5cb11accbaa18
iproduct <office@iproduct.org>
2017-03-13 11:37:12 +0200

    LineFollower01 oscilation seeking line

--- motors_demo/src/motors_demo/LineFollower01.java
+++ motors_demo/src/motors_demo/LineFollower01.java
@@ -16,40 +16,46 @@ import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.Color;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
+import static motors_demo.Direction.*;
 
 public class LineFollower01 {
+	private final EV3 ev3 = (EV3) BrickFinder.getLocal();
+	private TextLCD lcd = ev3.getTextLCD();
+	private Keys keys = ev3.getKeys();
+	private EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
+	private EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
+	RegulatedMotor mA, mB, mC;
+	SensorMode color;
+	float[] colorSample;
+	SensorMode touch;
+	float[] sample;
 
-	public static void main(String[] args) {
-		final EV3 ev3 = (EV3) BrickFinder.getLocal();
-		TextLCD lcd = ev3.getTextLCD();
-		Keys keys = ev3.getKeys();
-//		Audio audio = ev3.getAudio();
-		
-		//Color sensor
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
-		SensorMode color = colorSensor.getRGBMode();
-		float[] colorSample = new float[color.sampleSize()];
-		
-//		playMessage(audio);
-
-		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
-		SensorMode touch = touchSensor.getTouchMode();
-		float[] sample = new float[touch.sampleSize()];
-
-		RegulatedMotor mA, mB, mC;
+	public LineFollower01() {
 		mA = new EV3LargeRegulatedMotor(MotorPort.A);
 		mB = new EV3LargeRegulatedMotor(MotorPort.B);
 		mC = new EV3LargeRegulatedMotor(MotorPort.C);
 		mB.setSpeed(120);// 2 RPM
 		mC.setSpeed(120);
 		mB.synchronizeWith(new RegulatedMotor[] { mC });
-		
+		color = colorSensor.getRGBMode();
+		colorSample = new float[color.sampleSize()];
+		touch = touchSensor.getTouchMode();
+		sample = new float[touch.sampleSize()];
+	}
+
+	public void start() {
+		// Audio audio = ev3.getAudio();
+
+		// Color sensor
+
+		// playMessage(audio);
+
 		long startTime = System.currentTimeMillis();
 		long duration;
 		int lastColorId = Color.NONE;
 
-//		for (int i = 0; i < 4; i++) {
-
+		// for (int i = 0; i < 4; i++) {
+		do {
 			// go forward
 			mB.startSynchronization();
 			mB.forward();
@@ -64,32 +70,31 @@ public class LineFollower01 {
 				lcd.drawString("" + colorSample[0], 0, 3);
 				lcd.drawString("" + colorSample[1], 0, 4);
 				lcd.drawString("" + colorSample[2], 0, 5);
-			} while (duration < 60000 && mB.isMoving() && mC.isMoving()
-					&& sample[0] == 0 && isReflecting(colorSample)
+			} while (duration < 120000 && mB.isMoving() && mC.isMoving() && sample[0] == 0 && isReflecting(colorSample)
 					&& isRed(colorSample));
 			mB.startSynchronization();
 			mB.stop();
 			mC.stop();
 			mB.endSynchronization();
-			
-			//Seek the red line 
-//			seekRed();
 
-//			// go back
-//			mB.startSynchronization();
-//			mB.backward();
-//			mC.backward();
-//			mB.endSynchronization();
-//
-//			// turn back
-//			mB.startSynchronization();
-//			mB.rotate(1000, true);
-//			mC.rotate(-1000, true);
-//			mB.endSynchronization();
-//			while (mB.isMoving() && mC.isMoving())
-//				Thread.yield();
+			// Seek the red line
+			seekRed();
+		} while (duration < 120000 && mB.isMoving() && mC.isMoving() && sample[0] == 0 && isReflecting(colorSample));
+		// // go back
+		// mB.startSynchronization();
+		// mB.backward();
+		// mC.backward();
+		// mB.endSynchronization();
+		//
+		// // turn back
+		// mB.startSynchronization();
+		// mB.rotate(1000, true);
+		// mC.rotate(-1000, true);
+		// mB.endSynchronization();
+		// while (mB.isMoving() && mC.isMoving())
+		// Thread.yield();
 
-//		}
+		// }
 
 		mA.close();
 		mB.close();
@@ -115,12 +120,48 @@ public class LineFollower01 {
 		// lcd.drawInt(angle, 0, 2);
 		// lcd.drawInt((int)duration, 0, 1);
 		// keys.waitForAnyEvent();
-		
-//		getColor(lcd, color, colorSample);
-//		keys.waitForAnyPress();
+
+		// getColor(lcd, color, colorSample);
+		// keys.waitForAnyPress();
+
 	}
 
-	private static void playMessage(final Audio audio) {
+	public static void main(String[] args) {
+		LineFollower01 lineFollower = new LineFollower01();
+		lineFollower.start();
+	}
+
+	private void seekRed() {
+		long treshold = 500;
+		Direction direction = Direction.LEFT;
+		long seekStratTime = System.currentTimeMillis();
+		long seekDuration;
+		do {
+			long startRotationTime = System.currentTimeMillis();
+			treshold *= 1.2;
+			direction = (direction == LEFT) ? RIGHT : LEFT;
+			long duration;
+			do {
+				// turn
+				mB.startSynchronization();
+				if (direction == LEFT) {
+					mB.forward();
+					mC.backward();
+				} else {
+					mB.backward();
+					mC.forward();
+				}
+				mB.endSynchronization();
+				duration = System.currentTimeMillis() - startRotationTime;
+				color.fetchSample(colorSample, 0);
+				touch.fetchSample(sample, 0);
+			} while (duration < treshold && !isRed(colorSample));
+			seekDuration = seekStratTime - System.currentTimeMillis();
+		} while (seekDuration < 10000 && mB.isMoving() && mC.isMoving() && sample[0] == 0 && isReflecting(colorSample)
+				&& !isRed(colorSample));
+	}
+
+	private void playMessage(final Audio audio) {
 		Thread t = new Thread() {
 			@Override
 			public void run() {
@@ -132,19 +173,15 @@ public class LineFollower01 {
 		Delay.msDelay(6000);
 		t.interrupt();
 	}
-	
-	private static boolean isRed(float[] colorSample){
-		return colorSample[0] > 0.05 
-				&& colorSample[1] < 0.04 
-				&& colorSample[2] < 0.04;
+
+	private boolean isRed(float[] colorSample) {
+		return colorSample[0] > 0.12 && colorSample[1] < 0.04 && colorSample[2] < 0.04;
 	}
 
-	private static boolean isReflecting(float[] colorSample){
-		return colorSample[0] > 0.015 
-				|| colorSample[1] > 0.015 
-				|| colorSample[2] > 0.015;
+	private boolean isReflecting(float[] colorSample) {
+		return colorSample[0] > 0.015 || colorSample[1] > 0.015 || colorSample[2] > 0.015;
 	}
-	
-//	private static boolean findRedTrack()
+
+	// private static boolean findRedTrack()
 
 }
