3349cdeca449d0bf58b28f5fe40672a09b12a22c
Trayan Iliev <office@iproduct.org>
2021-04-07 20:40:25 +0300

    motor driver demo added

--- 05-Arduino/esp32-wroover-motor-driver-pwm/esp32-wroover-motor-driver-pwm.ino
+++ 05-Arduino/esp32-wroover-motor-driver-pwm/esp32-wroover-motor-driver-pwm.ino
@@ -1,17 +1,22 @@
 #include <Arduino.h>
 #include <analogWrite.h>
+int DRV_A_IN1 = 26
+int DRV_A_IN2 = 27
+int DRV_A_PWM = 14;
 
-int DRV_B_IN1 = 32; //define the LED connect to Pin11;
-int DRV_B_IN2 = 33; //define the LED connect to Pin11;
-int DRV_A_PWM = 25; //define the LED connect to Pin11;
-int DRV_B_PWM = 26; //define the LED connect to Pin11;
+int DRV_B_IN1 = 32; 
+int DRV_B_IN2 = 33; 
+int DRV_B_PWM = 25;
+
 int inpin = 34; //define the button connect to Pin7
 const unsigned long DURATION = 4000;
 const int LOOP_DELAY = 500;
 
+const int CLOCKWISE = 1;
+const int COUNTER_CLOCKWISE = 2;
 
 int val;//val to store the data
-boolean started;
+bool started;
 float speed = 0;
 float step;
 unsigned long switchonTime;
@@ -30,42 +35,57 @@ void setup()
 void loop()
 {
   val = digitalRead(inpin); //digital read the button pin
-  if (val == LOW) //if the button pressed;
+  if (val == LOW) //if the button is not pressed;
   {
     trunMotorsOff();
     started = false;
   }
   else
   {
-    if (!strated) {
+    if (!started) {
       switchonTime = millis();
       started = true;
       speed = 0;
+      setMotorDirection(2, CLOCKWISE);
     }
     speed += step;
     if (speed < 0 || speed > 255) {
       step = -step;
     }
-    if(speed > 255){
+    if (speed > 255) {
       speed = 255;
     }
-    if(speed < 0){
+    if (speed < 0) {
       speed = 0;
     }
-    Serial.printf("Active Motor: %d, Step: %f, Brightness: %f\n", 2, step, speed);
+    Serial.printf("Active Motor: %d, Step: %f, Speed %f\n", 2, step, speed);
     setMotorSpeed(2, round(speed));
     delay(LOOP_DELAY);
   }
 }
 
 void trunMotorsOff() {
-  digitalWrite(DRV_B_IN1, LOW); //turn off the LED
-  digitalWrite(DRV_B_IN2, LOW); //turn off the LED
+  digitalWrite(DRV_B_IN1, LOW);
+  digitalWrite(DRV_B_IN2, LOW);
+}
+
+void setMotorDirection(int motor, int direction) {
+  switch (direction) {
+    case CLOCKWISE: 
+      digitalWrite(motor == 1 ? DRV_A_IN1: DRV_B_IN1, HIGH);
+      digitalWrite(motor == 1 ? DRV_A_IN2: DRV_B_IN2, LOW);
+      break;
+    case COUNTER_CLOCKWISE:       
+      digitalWrite(motor == 1 ? DRV_A_IN1: DRV_B_IN1, LOW);
+      digitalWrite(motor == 1 ? DRV_A_IN2: DRV_B_IN2, HIGH);
+      break;
+
+  }
 }
 
 void setMotorSpeed(int motor, int speed) {
   switch (motor) {
-    case 1: analogWrite(DRV_A_PWM, 0); break;
-    case 2: analogWrite(DRV_B_PWM, 0); break;
+    case 1: analogWrite(DRV_A_PWM, speed); break;
+    case 2: analogWrite(DRV_B_PWM, speed); break;
   }
 }
