f960686063e8fb4bf6bb907fd50437c67e8bad62
Trayan Iliev <office@iproduct.org>
2023-05-26 19:55:18 +0300

    FastAPI + aiocoap + WebSocket updated - adaptive speed correction and command sequences

--- fmi-2023-fastapi/websocket.py
+++ fmi-2023-fastapi/websocket.py
@@ -12,24 +12,23 @@ import logging
 import aiocoap
 import aiocoap.resource as resource
 
+SERVER_IP='192.168.1.100'
+WEBAPP_PORT=5000
+ROBOT_IP='192.168.1.101'
+
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("coap-server").setLevel(logging.INFO)
 
 coap_ctx: aiocoap.Context = None
 ws: WebSocket = None
 
-# req = Message(code=PUT, payload='', uri='coap://192.168.1.100/other/block')
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global coap_ctx
     coap_ctx = await start_coap_server()
-    # global ws
-    # ws = app.websocket('/ws')
     # coap_ctx = await aiocoap.Context.create_client_context()
     yield
     await coap_ctx.shutdown()
-    # await server_ctx.shutdown()
 
 
 app = FastAPI(lifespan=lifespan, debug=True)
@@ -43,15 +42,17 @@ html = """
     <body>
         <h1>WebSocket Chat</h1>
         <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
+            <input type="text" id="messageText" style="width:80%" />
             <button>Send</button>
         </form>
         <div>Distances: <span id="distances"></span></div>
+        <div>Speeds: <span id="speeds"></span></div>
         <ul id='messages'>
         </ul>
         <script>
             var ws = new WebSocket("ws://localhost:5000/ws");
             var distances = document.getElementById('distances')
+            var speeds = document.getElementById('speeds')
             var messages = document.getElementById('messages')
             ws.onmessage = function(message) {
                 const event = JSON.parse(message.data)
@@ -62,12 +63,14 @@ html = """
                     messages.appendChild(li)
                 } else if(event.type === 'distance') {
                     distances.innerHTML = `L: ${Math.round(event.distanceL)}, R: ${Math.round(event.distanceR)}`
+                } else if(event.type === 'move') {
+                    speeds.innerHTML = `encoderL: ${event.encoderL}, encoderR: ${event.encoderR}, speedL: ${event.speedL}, speedR: ${event.speedR}`
                 }
             };
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
                 ws.send(input.value)
-                input.value = ''
+                // input.value = ''
                 event.preventDefault()
             }
         </script>
@@ -112,8 +115,6 @@ class BlockResource(resource.Resource):
 
     def set_content(self, content):
         self.content = content
-        # while len(self.content) <= 1024:
-        #     self.content = self.content + b"0123456789\n"
 
     async def render_get(self, request):
         return aiocoap.Message(payload=self.content)
@@ -133,28 +134,13 @@ async def start_coap_server():
     root = resource.Site()
 
     root.add_resource(['.well-known', 'core'], resource.WKCResource(root.get_resources_as_linkheader))
-    # root.add_resource(['time'], TimeResource())
     root.add_resource(['sensors', 'distance'], BlockResource())
-    # root.add_resource(['other', 'separate'], SeparateLargeResource())
-    # root.add_resource(['whoami'], WhoAmI())
-    return await aiocoap.Context.create_server_context(root, bind=['192.168.1.100', 5683])
+    return await aiocoap.Context.create_server_context(root, bind=[SERVER_IP, 5683])
 
 
 async def send_command(message):
-    # ctx = await Context.create_client_context()
-
-    # request = Message(code=GET, uri='coap://192.168.1.100/time')
-    # request = Message(code=GET, uri='coap://192.168.1.100/other/block')
-    # request = Message(code=GET, uri='coap+ws://192.168.1.100/whoami')
-    # req = Message(code=GET, uri='coap+ws://192.168.1.100:8683/light')
-
-    # payload = b'1';
-
-    # req.remote = UndecidedRemote("coap", "192.168.1.101:5683")
-    # req.opt.proxy_scheme = 'coap'
-
-    # req = Message(code=PUT, payload=message.encode(encoding='utf-8'), uri='coap://192.168.1.101:5683/light')
-    req = aiocoap.Message(code=aiocoap.PUT, payload=message.encode(encoding='utf-8'), uri='coap://192.168.1.101:5683/light')
+    print(f'Sending to ESP32: {message}')
+    req = aiocoap.Message(code=aiocoap.PUT, payload=message.encode(encoding='utf-8'), uri='coap://' + ROBOT_IP + ':5683/light')
 
     try:
         response = await coap_ctx.request(req).response
@@ -164,42 +150,6 @@ async def send_command(message):
     else:
         print('Result: %s\n%r' % (response.code, response.payload.decode('utf8')))
         return response.payload.decode('utf8')
-    # finally:
-    #     await ctx.shutdown()
-
-
-# async def main():
-#     boo_task = asyncio.create_task(start_coap_server())
-#
-#     config = uvicorn.Config("websocket:app", port=5000, log_level="info", workers=10)
-#     server = uvicorn.Server(config)
-#     baa_task = asyncio.create_task(server.serve())
-#
-#     await boo_task
-#     await baa_task
 
 if __name__ == "__main__":
-    # asyncio.ensure_future(start_coap_server())
-    # executor = ProcessPoolExecutor(2)
-    # loop = asyncio.new_event_loop()
-    # boo = loop.run_in_executor(executor, start_coap_server())
-    #
-
-    # baa = loop.run_in_executor(executor, server.serve())
-    #
-    # loop.run_forever()
-
-    #run("websocket:app", port=5000, access_log=False, workers=1)
-    # baa = loop.run_in_executor(executor, say_baa)
-    #
-    # loop.run_forever()
-    #
-
-
-
-
-    # loop = asyncio.get_event_loop()
-    # loop.run_forever()
-
-    # asyncio.run(main())
-    uvicorn.run("websocket:app", port=5000, access_log=False, workers=1)
+    uvicorn.run("websocket:app", port=WEBAPP_PORT, access_log=False, workers=1)
--- fmi-2023-fastapi/websocket_old.py
+++ fmi-2023-fastapi/websocket_old.py
@@ -18,13 +18,18 @@ logging.getLogger("coap-server").setLevel(logging.INFO)
 coap_ctx: aiocoap.Context = None
 ws: WebSocket = None
 
+# req = Message(code=PUT, payload='', uri='coap://192.168.1.100/other/block')
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global coap_ctx
     coap_ctx = await start_coap_server()
+    # global ws
+    # ws = app.websocket('/ws')
     # coap_ctx = await aiocoap.Context.create_client_context()
     yield
     await coap_ctx.shutdown()
+    # await server_ctx.shutdown()
 
 
 app = FastAPI(lifespan=lifespan, debug=True)
@@ -107,6 +112,8 @@ class BlockResource(resource.Resource):
 
     def set_content(self, content):
         self.content = content
+        # while len(self.content) <= 1024:
+        #     self.content = self.content + b"0123456789\n"
 
     async def render_get(self, request):
         return aiocoap.Message(payload=self.content)
@@ -126,11 +133,27 @@ async def start_coap_server():
     root = resource.Site()
 
     root.add_resource(['.well-known', 'core'], resource.WKCResource(root.get_resources_as_linkheader))
+    # root.add_resource(['time'], TimeResource())
     root.add_resource(['sensors', 'distance'], BlockResource())
+    # root.add_resource(['other', 'separate'], SeparateLargeResource())
+    # root.add_resource(['whoami'], WhoAmI())
     return await aiocoap.Context.create_server_context(root, bind=['192.168.1.100', 5683])
 
 
 async def send_command(message):
+    # ctx = await Context.create_client_context()
+
+    # request = Message(code=GET, uri='coap://192.168.1.100/time')
+    # request = Message(code=GET, uri='coap://192.168.1.100/other/block')
+    # request = Message(code=GET, uri='coap+ws://192.168.1.100/whoami')
+    # req = Message(code=GET, uri='coap+ws://192.168.1.100:8683/light')
+
+    # payload = b'1';
+
+    # req.remote = UndecidedRemote("coap", "192.168.1.101:5683")
+    # req.opt.proxy_scheme = 'coap'
+
+    # req = Message(code=PUT, payload=message.encode(encoding='utf-8'), uri='coap://192.168.1.101:5683/light')
     req = aiocoap.Message(code=aiocoap.PUT, payload=message.encode(encoding='utf-8'), uri='coap://192.168.1.101:5683/light')
 
     try:
@@ -141,6 +164,42 @@ async def send_command(message):
     else:
         print('Result: %s\n%r' % (response.code, response.payload.decode('utf8')))
         return response.payload.decode('utf8')
+    # finally:
+    #     await ctx.shutdown()
+
+
+# async def main():
+#     boo_task = asyncio.create_task(start_coap_server())
+#
+#     config = uvicorn.Config("websocket:app", port=5000, log_level="info", workers=10)
+#     server = uvicorn.Server(config)
+#     baa_task = asyncio.create_task(server.serve())
+#
+#     await boo_task
+#     await baa_task
 
 if __name__ == "__main__":
+    # asyncio.ensure_future(start_coap_server())
+    # executor = ProcessPoolExecutor(2)
+    # loop = asyncio.new_event_loop()
+    # boo = loop.run_in_executor(executor, start_coap_server())
+    #
+
+    # baa = loop.run_in_executor(executor, server.serve())
+    #
+    # loop.run_forever()
+
+    #run("websocket:app", port=5000, access_log=False, workers=1)
+    # baa = loop.run_in_executor(executor, say_baa)
+    #
+    # loop.run_forever()
+    #
+
+
+
+
+    # loop = asyncio.get_event_loop()
+    # loop.run_forever()
+
+    # asyncio.run(main())
     uvicorn.run("websocket:app", port=5000, access_log=False, workers=1)
