166f7b3def4494ad09e4e1ffa99bae0acd51c8c8
Trayan Iliev <office@iproduct.org>
2024-03-29 19:15:06 +0200

    new ESP32 wifi conection demo

--- fmi-2024-03-esp32-network/fmi-2024-03-esp32-network.ino
+++ fmi-2024-03-esp32-network/fmi-2024-03-esp32-network.ino
@@ -14,31 +14,23 @@ void setup() {
 }
 
 void loop() {
- Serial.println("scan start");
+ WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
 
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print("): ");
-            Serial.print(WiFi.BSSIDstr(i));
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-            delay(10);
-        }
-    }
-    Serial.println("");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-    // Wait a bit before scanning again
-    delay(5000);
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
