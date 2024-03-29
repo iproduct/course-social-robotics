a978d548a248556c4fd2420a23d7d59738b0b647
Trayan Iliev <office@iproduct.org>
2024-03-29 19:17:06 +0200

    new ESP32 wifi conection demo

--- fmi-2024-03-esp32-network/fmi-2024-03-esp32-network.ino
+++ fmi-2024-03-esp32-network/fmi-2024-03-esp32-network.ino
@@ -5,16 +5,7 @@
 void setup() {
   Serial.begin(115200);
 
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  Serial.println("Setup done");
-}
-
-void loop() {
- WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
 
@@ -33,4 +24,10 @@ void loop() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+
+
+  Serial.println("Setup done");
+}
+
+void loop() {
 }
