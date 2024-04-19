ace9170afb5d3af4ab761dd7a39f78ff9ed42acc
Trayan Iliev <office@iproduct.org>
2024-04-19 20:54:09 +0300

    WS subscription added to api-client

--- fmi-2024-05-react-frontend/src/services/api-client.js
+++ fmi-2024-05-react-frontend/src/services/api-client.js
@@ -1,15 +1,32 @@
+import { Subject } from "rxjs";
 
 class ApiClient {
-    constructor(apiBaseUrl) {
+    constructor(apiBaseUrl, wsBaseUrl) {
         this.apiBaseUrl = apiBaseUrl;
+        this.wsBaseUrl = wsBaseUrl;
+    }
+
+    async init() {
+        this.ws = new WebSocket(this.wsBaseUrl);
+        this.readings = new Subject();
+        this.ws.onmessage = function(message) {
+            const event = JSON.parse(message.data);
+            this.readings.next(event);
+        };
+    }
+
+    sendMessage(event) {
+        var input = document.getElementById("messageText");
+        this.ws.send(input.value);
     }
 
     async findAllSensors() {
-       const resp =  await fetch(`${this.apiBaseUrl}/api/sensors`);
+       const resp =  await fetch(`${this.apiBaseUrl}/sensors`);
        return resp.json();
     }
 } 
 
-const API = new ApiClient('http://localhost:5000');
+const API = new ApiClient('http://localhost:5000/api', "ws://localhost:5000/ws");
+API.init();
 
 export default API;
