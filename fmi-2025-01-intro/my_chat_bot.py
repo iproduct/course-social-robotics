e57b3ac079a83e303d204c9da7826aa21ae52092
Trayan Iliev <office@iproduct.org>
2025-02-20 21:45:34 +0200

    chat with llama3.2

--- fmi-2025-01-intro/my_chat_bot.py
+++ fmi-2025-01-intro/my_chat_bot.py
@@ -1,9 +1,26 @@
+import ollama
+
+
 class ChatBot:
     def __init__(self, name, model):
-        print("--- Statring up ", name, " ---")
+        print("--- Statring up", name, " ---")
         self.name = name
         self.model = model
 
     def run(self):
-        ollama.chat(self.name, self.model)
+        resp = ollama.chat(self.model, messages=[
+            {
+                'role': 'system',
+                'content': 'You are an AI assistant',
+            },
+            {
+                'role': 'user',
+                'content': 'Why the sky is blue?',
+            }
+        ])
+        text = resp['message']['content']
+        print(text)
 
+if __name__ == '__main__':
+    maya = ChatBot("Maya", 'llama3.2')
+    maya.run()
