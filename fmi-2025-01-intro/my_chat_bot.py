661a35df7a6add55eb6867f7241036485111245a
Trayan Iliev <office@iproduct.org>
2025-02-20 21:55:44 +0200

    tts and stt added

--- fmi-2025-01-intro/my_chat_bot.py
+++ fmi-2025-01-intro/my_chat_bot.py
@@ -1,4 +1,8 @@
+from gtts import gTTS
 import ollama
+import speech_recognition as sr
+import sounddevice as sd
+import soundfile as sf
 
 
 class ChatBot:
@@ -6,6 +10,35 @@ class ChatBot:
         print("--- Statring up", name, " ---")
         self.name = name
         self.model = model
+        self.recognizer = sr.Recognizer()
+        self.messages = [
+            {
+                'role': 'system',
+                'content': """You are an AI assistant. Your name is Maya. 
+                               You are a spoiled russian girl with a nasty attitude and strong accent. 
+                               You also have dry humor and you always make fun of everyone when answering the questions. 
+                               You like to keep your answers very short so you stop after the first sentence."""
+            }
+        ]
+
+    def speech_to_text(self):
+        print('Listening ...')
+        with sr.Microphone() as mic:
+            self.recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+            audio = self.recognizer.listen(mic)
+        try:
+            text = self.recognizer.recognize_google(audio, language='en')
+            return text
+        except Exception as ex:
+            print("You -> Recognition Error", ex)
+
+
+    def text_to_speech(self, message):
+        audio_obj = gTTS(text=message, lang='en', slow=False)
+        audio_obj.save('output.mp3')
+        audio_arr, sample_rate = sf.read('output.mp3')
+        sd.play(audio_arr, sample_rate)
+        sd.wait()
 
     def run(self):
         resp = ollama.chat(self.model, messages=[
