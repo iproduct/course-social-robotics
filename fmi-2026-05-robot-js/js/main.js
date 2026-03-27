6a4a3ecf7a3b7bd5c784091d9100354d2df36969
Trayan Iliev <office@iproduct.org>
2026-03-27 17:45:03 +0200

    index.html form added

--- fmi-2026-05-robot-js/js/main.js
+++ fmi-2026-05-robot-js/js/main.js
@@ -1,13 +1,15 @@
 const ws = new WebSocket("ws://localhost:5000/ws");
 const messages = document.getElementById('messages')
+const input = document.getElementById("messageText")
+
 ws.onmessage = function (message) {
     const event = JSON.parse(message.data)
-    if (event.type === 'command_ack') {
-        var li = document.createElement('li')
-        var content = document.createTextNode(event.payload)
-        li.appendChild(content)
-        messages.appendChild(li)
-    } 
+    // if (event.type === 'command_ack') {
+    var li = document.createElement('li')
+    var content = document.createTextNode(event.payload)
+    li.appendChild(content)
+    messages.appendChild(li)
+    // } 
     // else if (event.type === 'distance') {
     //     distances.innerHTML += `${Math.round(event.distance)}, `
     // } else if (event.type === 'move') {
@@ -15,7 +17,6 @@ ws.onmessage = function (message) {
     // }
 };
 function sendMessage(event) {
-    const input = document.getElementById("messageText")
     ws.send(input.value)
     // input.value = ''
     event.preventDefault()
